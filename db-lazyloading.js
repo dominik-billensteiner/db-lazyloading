0a4cea2fe222ce8b055085c229699c2bd377de67
dominik-billensteiner <56152933+dominik-billensteiner@users.noreply.github.com>
2020-05-20 10:48:11 +0200

    Code rewrite to ES6.

--- db-lazyloading.js
+++ db-lazyloading.js
@@ -16,54 +16,56 @@ document.addEventListener("DOMContentLoaded", function () {
                     root: document.querySelector(".center"),
                     rootMargin: "0px 0px 200px 0px"
                 };
-                function onIntersection(imageEntites) {
+                var onIntersection = function (imageEntites) {
                     // Call function, when any image entity is in the intersection
                     imageEntites.forEach(function (img) {
-                        // Perform for every entity
+                        // intersectionRatio covers Browsers which do not suppport isIntersecting
                         if (img.isIntersecting || img.intersectionRatio > 0) {
-                            // If image is intersecting || intersectionRatio covers Browsers which do not suppport isIntersecting(e.g. Samsung Browser 5.x with Chromium 5x)
-                            observer.unobserve(img.target);
-                            // Set src to data-src, and srcset to datasrc-set, remove obsulete attributes
+                            // Stop observing the intersecting image
+                            observer_1.unobserve(img.target);
+                            // Display image instead of loading icon
                             img.target.src = img.target.dataset.src;
                             img.target.removeAttribute("data-src");
                             if (img.target.dataset.srcset != null) {
                                 img.target.srcset = img.target.dataset.srcset;
                                 img.target.removeAttribute("data-srcset");
                             }
+                            // Set loaded status to true if image has completlety loaded
                             img.target.onload = function () {
-                                // Save loaded status of image in a data property
                                 img.target.setAttribute("data-loaded", "true");
+                                // Add styles for appearing imgs
+                                img.target.classList.add("lazy-load--is-loaded");
                             };
-                            //img.target.classList.add("lazy-load--is-loaded"); to add styles for appearing imgs */
                         }
                     });
-                }
-                var observer = new IntersectionObserver(onIntersection, options); // Instanciate observer
-                for (var i = 0; i < imgs.length; i++) {
-                    observer.observe(imgs[i]);
-                }
-                //lazyImgs.map(img => observer.observe(img)); // Register for every image with lazy loading class attribute
+                };
+                // Instanciate observer
+                var observer_1 = new IntersectionObserver(onIntersection, options);
+                // Register IntersectionObserver on images
+                imgs.forEach(function (img) {
+                    observer_1.observe(img);
+                });
             }
             else
-                lazyLoadingFallback(imgs); // IntersectionObserver not available, fallback to simply loading images
+                showAllImages(imgs); // IntersectionObserver not available, fallback to simply loading images
         }
         catch (e) {
             // Catch exception if IntersectionObserver causes referencenotfound error e.g. in older safari browsers
-            lazyLoadingFallback(imgs);
+            showAllImages(imgs);
         }
     }
 });
 /**
- * Fallback if error coccurs with lazy loading function, sets all src attribute of images to value of data-src
+ * Shows all images by setting src attribute.
  *
- * @param  {Array} - Array of all images, which are loading lazy.
+ * @param  {any} imgs - NodeList of all images, which are loading lazy.
  */
-function lazyLoadingFallback(lazyImgs) {
+function showAllImages(imgs) {
     // Go through all grid image
-    if (lazyImgs != null) {
-        for (var i = 0; i < lazyImgs.length; i++) {
+    if (imgs) {
+        imgs.forEach(function (img) {
             // Set attribute src to value of data-src
-            lazyImgs[i].setAttribute("src", lazyImgs[i].getAttribute("data-src"));
-        }
+            img.setAttribute("src", img.getAttribute("data-src"));
+        });
     }
 }
